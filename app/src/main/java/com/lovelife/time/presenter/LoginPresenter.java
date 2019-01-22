d1c8e26a568d486a4d543fa30e21fa099282d023
tianyouliangliao <fengzhangwei399@yeah.net>
2019-01-22 19:07:07 +0800

    未解决权限

--- app/src/main/java/com/lovelife/time/presenter/LoginPresenter.java
+++ app/src/main/java/com/lovelife/time/presenter/LoginPresenter.java
@@ -8,7 +8,12 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.lovelife.time.base.BasePresenter;
+import com.lovelife.time.bean.BaseCallModel;
+import com.lovelife.time.bean.HomeCarBean;
 import com.lovelife.time.contract.LoginContract;
+import com.lovelife.time.internet.ApiService;
+import com.lovelife.time.internet.RetrofitManager;
+import com.lovelife.time.utlis.RxSchedulers;
 import com.tencent.connect.UserInfo;
 import com.tencent.connect.auth.QQToken;
 import com.tencent.tauth.IUiListener;
@@ -19,6 +24,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import javax.inject.Inject;
+
+import io.reactivex.functions.Consumer;
 
 public class LoginPresenter extends BasePresenter<LoginContract.View> implements LoginContract.Presenter {
 
@@ -35,25 +42,6 @@ public class LoginPresenter extends BasePresenter<LoginContract.View> implements
     }
 
 
-//    @Override
-//    public void getData() {
-//        RetrofitManager.create(ApiService.class)
-//                .getCarData(1)
-//                .compose(RxSchedulers.<BaseCallModel<HomeCarBean>>applySchedulers())
-//                .compose(mView.<BaseCallModel<HomeCarBean>>bindToLife())
-//                .subscribe(new Consumer<BaseCallModel<HomeCarBean>>() {
-//                    @Override
-//                    public void accept(BaseCallModel<HomeCarBean> homeCarBeanBaseCallModel) throws Exception {
-//                             mView.onSuccess();
-//                    }
-//                }, new Consumer<Throwable>() {
-//                    @Override
-//                    public void accept(Throwable throwable) throws Exception {
-//                             mView.onError();
-//
-//                    }
-//                });
-//    }
 
     @Override
     public void getData(Context context, String p) {
@@ -62,6 +50,22 @@ public class LoginPresenter extends BasePresenter<LoginContract.View> implements
         if (TextUtils.isEmpty(p)) return;
         uiListener = new BaseUiListener();
         tencent.login((Activity) context, p, uiListener);
+//        RetrofitManager.create(ApiService.class)
+//                .getCarData(1)
+//                .compose(RxSchedulers.<BaseCallModel<HomeCarBean>>applySchedulers())
+//                .compose(mView.<BaseCallModel<HomeCarBean>>bindToLife())
+//                .subscribe(new Consumer<BaseCallModel<HomeCarBean>>() {
+//                    @Override
+//                    public void accept(BaseCallModel<HomeCarBean> homeCarBeanBaseCallModel) throws Exception {
+//
+//                    }
+//                }, new Consumer<Throwable>() {
+//                    @Override
+//                    public void accept(Throwable throwable) throws Exception {
+//
+//
+//                    }
+//                });
     }
 
     @Override
--- app/src/main/java/com/lovelife/time/view/activity/SplashActivity.java
+++ app/src/main/java/com/lovelife/time/view/activity/SplashActivity.java
@@ -2,9 +2,15 @@ package com.lovelife.time.view.activity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AppOpsManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.widget.ImageView;
@@ -25,6 +31,9 @@ import com.lovelife.time.app.DHApplication;
 import com.lovelife.time.base.BaseActivity;
 import com.lovelife.time.utlis.SPUtils;
 import com.lovelife.time.weight.SPKey;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class SplashActivity extends BaseActivity {
 
@@ -91,16 +100,25 @@ public class SplashActivity extends BaseActivity {
             @Override
             public void permissionGranted(@NonNull String[] permission) {
                 Intent mIntent ;
-                String s = SPUtils.getInstance(SplashActivity.this).getString(SPKey.USER_ID);
-                if (TextUtils.isEmpty(s)){
-                 mIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                boolean b = checkFloatPermission(SplashActivity.this);
+                if (!b){
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                            Uri.parse("package:" + getPackageName()));
+                    startActivityForResult(intent,10);
                 }else {
-                 mIntent = new Intent(SplashActivity.this,MainActivity.class);
+                    String s = SPUtils.getInstance(SplashActivity.this).getString(SPKey.USER_ID);
+                    if (TextUtils.isEmpty(s)){
+                        mIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                    }else {
+                        mIntent = new Intent(SplashActivity.this,MainActivity.class);
+                    }
+                    startActivity(mIntent);
+                    DHApplication.getDao().createTab();
+                    finish();
+                    overridePendingTransition(R.anim.anim_in,R.anim.anim_out);
                 }
-                startActivity(mIntent);
-                DHApplication.getDao().createTab();
-                finish();
-                overridePendingTransition(R.anim.anim_in,R.anim.anim_out);
+
+
             }
 
             @Override
@@ -110,9 +128,58 @@ public class SplashActivity extends BaseActivity {
         },permissions,false,null);
     }
 
+    public static boolean checkFloatPermission(Context context) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT)
+            return true;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            try {
+                Class cls = Class.forName("android.content.Context");
+                Field declaredField = cls.getDeclaredField("APP_OPS_SERVICE");
+                declaredField.setAccessible(true);
+                Object obj = declaredField.get(cls);
+                if (!(obj instanceof String)) {
+                    return false;
+                }
+                String str2 = (String) obj;
+                obj = cls.getMethod("getSystemService", String.class).invoke(context, str2);
+                cls = Class.forName("android.app.AppOpsManager");
+                Field declaredField2 = cls.getDeclaredField("MODE_ALLOWED");
+                declaredField2.setAccessible(true);
+                Method checkOp = cls.getMethod("checkOp", Integer.TYPE, Integer.TYPE, String.class);
+                int result = (Integer) checkOp.invoke(obj, 24, Binder.getCallingUid(), context.getPackageName());
+                return result == declaredField2.getInt(cls);
+            } catch (Exception e) {
+                return false;
+            }
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                AppOpsManager appOpsMgr = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+                if (appOpsMgr == null)
+                    return false;
+                int mode = appOpsMgr.checkOpNoThrow("android:system_alert_window", android.os.Process.myUid(), context
+                        .getPackageName());
+                return mode == AppOpsManager.MODE_ALLOWED || mode == AppOpsManager.MODE_IGNORED;
+            } else {
+                return Settings.canDrawOverlays(context);
+            }
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         handler.removeCallbacksAndMessages(null);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 10) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    // SYSTEM_ALERT_WINDOW permission not granted...
+                    Toast.makeText(SplashActivity.this,"请授予权限",Toast.LENGTH_SHORT);
+                    finish();
+                }
+            }
+        }
     }
 }
